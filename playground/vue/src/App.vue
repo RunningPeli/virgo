d38126cdb73ab37860dfa0abf4114211ad89513b
Futó Mirkó <futomirko@gmail.com>
2025-03-09 14:45:48 +0100

    Initialize playground Vue 3 project with TypeScript, Vite, and Tailwind CSS setup

---
+++ playground/vue/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { VirgoButton } from '@runningpeli/virgo'
+</script>
+
+<template>
+  <div>
+    <h1>Playground for Virgo</h1>
+    <VirgoButton class="bg-teal-500 px-2 py-1 rounded-md text-white" text="Button" />
+  </div>
+</template>
